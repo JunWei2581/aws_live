29845f5ab3e3d13ffcad27ae123b45eb339cc869
JunWei2581 <lowcw-wm24@student.tarc.edu.my>
2026-08-20 15:26:24 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.cy57nymlq25u.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "lowchunwei-employee"
+customregion = "us-east-1"
+
